cf14f8b8482f9632b9e7c3c412b81bac4f5bbc69
Ranjan-RR <ranjankumarsah2477@gmail.com>
2024-05-02 22:09:09 +0530

    Add file

---
+++ home.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- swiper css link -->
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
+    <!--font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<section class="header">
+    <a href="home.php" class="logo">Journey joy</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php" target="black">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>    
+    </nav>
+    
+    <!-- <i id="menu-btn" class="fa-solid fa-bars"></i>  -->
+    
+    <div id="menu-btn" class="fas fa-bars"></div>
+    
+
+</section>    
+
+<!-- home section -->
+
+<section class="home">
+
+    <div class="swiper home-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide slide" style="background:url(home-img2.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3>travel arround the world</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(home-img6.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3>discover the new places</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(home-img3.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3>make your tour worthwhile</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+
+    </div>
+
+</section>
+
+
+
+<!-- services starts -->
+
+
+<section class="services1">
+    
+    <h1 class="heading-title">our services</h1>
+    <div class="box-container1">
+
+        <div class="box1">
+            <img src="mountain1.png" alt="">
+            <h3>adventure</h3>
+        </div>
+
+        <div class="box1">
+            <img src="guide1.png" alt="">
+            <h3>tour guide</h3>
+        </div>
+
+        <div class="box1">
+            <img src="trekking1.png" alt="">
+            <h3>trekking</h3>
+        </div>
+
+        <div class="box1">
+            <img src="bonfire1.png" alt="">
+            <h3>camp fire</h3>
+        </div>
+        
+        <div class="box1">
+            <img src="jeep1.png" alt="">
+            <h3>off road</h3>
+        </div>
+
+        <div class="box1">
+            <img src="camping1.png" alt="">
+            <h3>camping</h3>
+        </div>
+
+
+       
+
+      
+
+
+
+    
+
+
+
+
+
+    </div>
+
+</section>
+
+ <!-- home about section -->
+
+<section class="home-about">
+
+<div class="image">
+    <img src="home-img.webp" alt="">
+</div>
+
+<div class="content">
+    <h3>about us</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, modi? Ipsa, voluptatum nam sapiente ex vitae voluptas eveniet, maiores voluptates aut omnis, beatae at unde. Animi error perspiciatis quae</p>
+
+    <a href="about.php" class="btn">read more</a>
+
+</div>
+
+</section>
+
+
+
+<!-- home package section -->
+
+<section class="home-packages">
+<h1 class="heading-title">our packages</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="package-img1.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Maldives</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quas cumque? Voluptatem, natus odio. Vel quasi hic a soluta natus consequatur dignissimos consequuntur reprehenderit alias, blanditiis, numquam impedit possimus explicabo?</p>
+
+                <span>₹ 11,999</span>
+
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+    
+        <div class="box">
+            <div class="image">
+                <img src="img4.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>advanture & tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quas cumque? Voluptatem, natus odio. Vel quasi hic a soluta natus consequatur dignissimos consequuntur reprehenderit alias, blanditiis, numquam impedit possimus explicabo?</p>
+
+                <span>₹ 11,999</span>
+
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+
+    
+        <div class="box">
+            <div class="image">
+                <img src="img4.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>advanture & tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quas cumque? Voluptatem, natus odio. Vel quasi hic a soluta natus consequatur dignissimos consequuntur reprehenderit alias, blanditiis, numquam impedit possimus explicabo?</p>
+
+                <span>₹ 11,999</span>
+
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="load-more"> <a href="package.php" class="btn">load more</a></div>
+</section>
+
+
+
+<!-- home offer section -->
+
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi mollitia distinctio perferendis voluptatem aspernatur adipisci, praesentium perspiciat</p>
+        <a href="book.php" class="btn">book now</a>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+<!-- footer section  -->
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>book</a>  
+
+    </div>
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>term of use</a>  
+
+    </div>
+    
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
+        <a href="#"><i class="fas fa-phone"></i>+111-222-3333</a>
+        <a href="#"><i class="fas fa-envelope"></i>ranjankumar@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i>Bihar, india-800004</a>
+
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+        <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+        <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>  
+
+    </div>
+
+</div>
+
+<div class="credit">created by <span>our group</span> All Right Reserved</div>
+
+
+</section>
+
+
+
+
+    <!-- swiper js link -->
+    
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> 
+
+    <script src="script.js"></script>
+    
+</body>
+</html>
